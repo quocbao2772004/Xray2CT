0e1b47eaa0811d1142f768c49a47dd8c699a767e
Lê Trần Quốc Bảo <123377752+quocbao2772004@users.noreply.github.com>
2024-11-06 17:35:13 +0700

    Update visualize.py

--- aritra_project/visualize.py
+++ aritra_project/visualize.py
@@ -9,7 +9,7 @@ def my_vis(epoch_values, loss_values, val_loss_values, metric_values, val_metric
     ax1.plot(epoch_values, loss_values)
     ax1.plot(epoch_values, val_loss_values)
     ax1.legend(['Train', 'Val'])
-    plt.savefig('/home/daisylabs/aritra_project/results/loss.png')
+    plt.savefig('/teamspace/studios/this_studio/Reconstruction-of-3D-CT-Volume-from-2D-X-ray-Images-using-Deep-Learning/aritra_project/my_dataset_results/loss.png')
     plt.show()
 
     fig2, ax2 = plt.subplots()
@@ -19,7 +19,7 @@ def my_vis(epoch_values, loss_values, val_loss_values, metric_values, val_metric
     ax2.plot(epoch_values, metric_values)
     ax2.plot(epoch_values, val_metric_values)
     ax2.legend(['Train', 'Val'])
-    plt.savefig('/home/daisylabs/aritra_project/results/PSNR.png')
+    plt.savefig('/teamspace/studios/this_studio/Reconstruction-of-3D-CT-Volume-from-2D-X-ray-Images-using-Deep-Learning/aritra_project/my_dataset_results/PSNR.png')
     plt.show()
 
     fig3, ax3 = plt.subplots()
@@ -29,19 +29,19 @@ def my_vis(epoch_values, loss_values, val_loss_values, metric_values, val_metric
     ax3.plot(epoch_values, metric1_values)
     ax3.plot(epoch_values, val_metric1_values)
     ax3.legend(['Train', 'Val'])
-    plt.savefig('/home/daisylabs/aritra_project/results/SSIM.png')
+    plt.savefig('/teamspace/studios/this_studio/Reconstruction-of-3D-CT-Volume-from-2D-X-ray-Images-using-Deep-Learning/aritra_project/my_dataset_results/SSIM.png')
     plt.show()
 
     op_st_dct = output.state_dict()
 
-    torch.save(op_st_dct, '/home/daisylabs/aritra_project/results/output.pth')
+    torch.save(op_st_dct, '/teamspace/studios/this_studio/Reconstruction-of-3D-CT-Volume-from-2D-X-ray-Images-using-Deep-Learning/aritra_project/my_dataset_results/output.pth')
 
     if(best_metric_coeff==1):
-        torch.save(op_st_dct, '/home/daisylabs/aritra_project/results/output_best.pth')
+        torch.save(op_st_dct, '/teamspace/studios/this_studio/Reconstruction-of-3D-CT-Volume-from-2D-X-ray-Images-using-Deep-Learning/aritra_project/my_dataset_results/output_best.pth')
         print('Best Output State Updated')
     else:
         print('Best Output State Retained')
 
-    return
+    # return
 
 
